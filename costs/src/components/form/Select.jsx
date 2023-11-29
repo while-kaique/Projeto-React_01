595079834d15609cc6112506d5eeb7931d699b4c
while-kaique <123902283+while-kaique@users.noreply.github.com>
2023-11-29 01:32:00 -0300

    Formulário finalizado!

--- costs/src/components/form/Select.jsx
+++ costs/src/components/form/Select.jsx
@@ -5,9 +5,10 @@ function Select ({text, name, options, handleOnChange, value}){
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
-                <option>{options.map((option)=>{
+                <option value="">Selecione uma opção</option>
+                {options.map((option)=>{
                     return <option value={option.id} key={option.id}>{option.name}</option>
-                })}</option>
+                })}
             </select>
         </div>
     )
