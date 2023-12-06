ec9e4462bbbd5a7680c68031b4e24873d94532f2
while-kaique <123902283+while-kaique@users.noreply.github.com>
2023-12-06 00:34:09 -0300

    Mensagem de novo projeto

--- costs/src/components/pages/NewProject.jsx
+++ costs/src/components/pages/NewProject.jsx
@@ -21,7 +21,8 @@ function NewProject (){
         }).then(res => res.json())
         .then(data => {
             console.log(data)
-            navigate("/projects",{ message: "Projeto criado com sucesso"})
+            const state = {message: "Projeto criado com sucesso"}
+            navigate("/projects",{ state})
         })
         .catch(err => console.log(err))
     }
--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,6 +1,20 @@
+import Message from "../layout/Message"
+import { useLocation } from "react-router-dom"
+
 function Projects (){
+
+    const location = useLocation()
+
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
     return (
-        <h1>Projects</h1>
+        <div>
+            <h1>Meu Projeto</h1>
+            {message && <Message type="sucess" msg={message}/>}
+        </div>
     )
 }
 
