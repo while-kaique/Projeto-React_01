91c318679cc65d780015c2384fdd09d3a930c46e
while-kaique <123902283+while-kaique@users.noreply.github.com>
2023-12-30 01:19:02 -0300

    Atualizando página de edição
    Básico

--- costs/src/components/pages/Project.jsx
+++ costs/src/components/pages/Project.jsx
@@ -2,28 +2,50 @@ import styles from './Project.module.css'
 
 import {useParams} from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import Loading from '../layout/Loading'
+
+
+import Container from '../layout/Container.jsx'
 
 function Project () {
 
     const { id } = useParams()
     
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
 
     useEffect(()=> {
-        fetch(`http://localhost:5000/projects/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'aplication/json'
-            }
-        }).then(resp => resp.json())
-        .then((data) => {
-             setProject(data)
-        })
-        .catch(err => console.log(err))
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'aplication/json'
+                }
+            }).then(resp => resp.json())
+            .then((data) => {
+                setProject(data)
+            })
+            .catch(err => console.log(err))
+        }, 500)    
     }, [id])
 
+    function toggleProjectForm(){
+
+    }
+
     return (
-        <p>{project.name}</p>
+        <> 
+            {project.name ? 
+                (<div>
+                    <Container customClass="column">
+                        <div>
+                            <h1>Projeto: {project.name}</h1>
+                            <button onClick={toggleProjectForm}>Editar Projeto</button>
+                        </div>
+                    </Container>
+                </div>)
+            : (<Loading />)}
+        </>
     )
 }
 
